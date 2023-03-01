75867287ff5f62f9441e047e43325ae1a0a897df
Ed Ivimey-Cook <e.ivimeycook@gmail.com>
2023-03-01 17:01:48 +0000

    Update and rename SD_AnalysisCode.R to dataExtraction_AnalysisCode.R

--- dataExtraction_AnalysisCode.R
+++ dataExtraction_AnalysisCode.R
@@ -8,13 +8,13 @@ library(patchwork)
 library(ggthemes)
 library(extrafont)
 
-sank <- read_csv("shinyDigitise_Analysis.csv") %>%
+MD_dat <- read_csv("metaDigitiseExtraction_Analysis.csv") %>%
   janitor::clean_names() %>%
   filter(published == "Published") %>%
   filter(paper_type == "Meta-analysis")
 
 
-g1<-ggplot(sank, aes(x = fct_rev(data_present), fill = data_present)) +
+g1<-ggplot(MD_dat, aes(x = fct_rev(data_present), fill = data_present)) +
   geom_bar() +
   theme_pilot() + 
   theme(legend.position = "none",
@@ -25,7 +25,7 @@ g1<-ggplot(sank, aes(x = fct_rev(data_present), fill = data_present)) +
   theme(plot.title = element_text(size = 12, face = "bold")) +
   scale_y_continuous(limits = c(0, 60), breaks = seq(0, 60, by = 20))
 
-g2<-ggplot(sank, aes(x = fct_rev(location_given), fill = location_given)) +
+g2<-ggplot(MD_dat, aes(x = fct_rev(location_given), fill = location_given)) +
   geom_bar() +
   theme_pilot() + 
   theme(legend.position = "none",
@@ -37,7 +37,7 @@ g2<-ggplot(sank, aes(x = fct_rev(location_given), fill = location_given)) +
   scale_y_continuous(limits = c(0, 60), breaks = seq(0, 60, by = 20))
 
 
-g3<-ggplot(sank, aes(x = fct_rev(figures_present), fill = figures_present)) +
+g3<-ggplot(MD_dat, aes(x = fct_rev(figures_present), fill = figures_present)) +
   geom_bar() +
   theme_pilot() + 
   theme(legend.position = "none") +
@@ -48,7 +48,7 @@ g3<-ggplot(sank, aes(x = fct_rev(figures_present), fill = figures_present)) +
   scale_y_continuous(limits = c(0, 60), breaks = seq(0, 60, by = 20))
 
 
-g4<-ggplot(sank, aes(x = fct_rev(caldat), fill = caldat)) +
+g4<-ggplot(MD_dat, aes(x = fct_rev(caldat), fill = caldat)) +
   geom_bar() +
   theme_pilot() + 
   theme(legend.position = "none") +
@@ -64,4 +64,4 @@ layout <-
 
 g1+g2+g3+g4 + plot_layout(design = layout)
 
-ggsave("bar.jpg", width = 8, height = 8, dpi = 600, device = "jpg")
+ggsave("MD_bars.jpg", width = 8, height = 8, dpi = 600, device = "jpg")
